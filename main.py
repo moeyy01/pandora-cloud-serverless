7d3afec78b6dce1a5dccb6ad6e69ed013e082286
moeyys <57945487+moeyy01@users.noreply.github.com>
2023-06-15 22:15:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,9 @@ from pandora_cloud.server import ChatBot
 
 _port = getenv('PORT')
 _proxy = getenv('PANDORA_PROXY')
-_apiprefix = getenv('CHATGPT_API_PREFIX', 'https://pandora-chat-api.moeyy.cn')
 _debug = getenv('PANDORA_DEBUG', 'false').lower() == 'true'
 _sentry = getenv('PANDORA_SENTRY', 'false').lower() == 'true'
 _listen = getenv('PANDORA_SERVER_LISTEN', 'true').lower() == 'true'
 _server = getenv('PANDORA_SERVER', '0.0.0.0:{}'.format(_port) if _port else '0.0.0.0')
 
-app = ChatBot(proxy=_proxy, debug=_debug, sentry=_sentry, login_local=True, api_prefix=_apiprefix).run(_server, listen=_listen)
+app = ChatBot(proxy=_proxy, debug=_debug, sentry=_sentry, login_local=True, api_prefix="https://pandora-chat-api.moeyy.cn").run(_server, listen=_listen)
